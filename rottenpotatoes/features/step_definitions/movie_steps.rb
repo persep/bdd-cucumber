83c2a6766ed7fe5156cd8e783348dfbac795668a
PerseP <persep@gmail.com>
2014-06-03 11:59:48 +0200

    Add restrict to movies filter

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -29,7 +29,16 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  flunk "Unimplemented"
+  # rating_list = "PG, R"
+  # uncheck = nil or "un"
+  
+  rating_list.split(/,\s*/).each { |rating|  # , one or more spaces characters
+    if uncheck
+      uncheck "ratings_#{rating}" # checkboxes id = ratings_G
+    else
+      check "ratings_#{rating}"
+    end
+  }
 end
 
 Then /I should see all the movies/ do
